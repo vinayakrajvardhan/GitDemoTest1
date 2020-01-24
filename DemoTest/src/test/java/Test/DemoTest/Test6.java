b0cd6c4dc50852d633ba6d473dfdc104f81ede7b
pankajgupta <pgupta584@gmail.com>
2020-01-24 22:10:40 +0530

    User 2 changes

---
+++ DemoTest/src/test/java/Test/DemoTest/Test6.java
@@ -0,0 +1,11 @@
+package Test.DemoTest;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello New Change");
+		System.out.println("Hello New Change 2");
+
+	}
+}
