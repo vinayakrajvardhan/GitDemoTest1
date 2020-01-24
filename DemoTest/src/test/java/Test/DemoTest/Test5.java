11c9012546f5e5a5309805a51bf320a5c96c15d2
pankajgupta <pgupta584@gmail.com>
2020-01-24 21:58:31 +0530

    Test1

---
+++ DemoTest/src/test/java/Test/DemoTest/Test5.java
@@ -0,0 +1,10 @@
+package Test.DemoTest;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello New Change");
+
+	}
+}
